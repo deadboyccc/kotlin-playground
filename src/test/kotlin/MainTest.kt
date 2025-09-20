a42c9c0db346c1efdf6533438f7d426664d30ef4
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-20 13:37:56 +0300

    demo test

---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class MainTest {
+    @BeforeEach
+    fun setUp() {
+
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun main() {
+        assertEquals("Hello World", "Hello World");
+    }
+
+}
