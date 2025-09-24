e70f79bac8f6851e45313187bd0baaaee80d8c3d
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-24 17:55:11 +0300

    errata + source code ( publicly available from Manning - Dont' sue me - ) XD

---
+++ src/main/kotlin/kotlin-in-action-2e-main/src/main/kotlin/ch07/7.1.8_2_NonnullTypesWithoutImmediateInitializationLateinitializedProperties1.kt
@@ -0,0 +1,23 @@
+package ch07.ex1_8_2_NonnullTypesWithoutImmediateInitializationLateinitializedProperties1
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
+class MyService {
+    fun performAction(): String = "Action Done!"
+}
+
+class MyTest {
+    private lateinit var myService: MyService
+
+    @BeforeAll
+    fun setUp() {
+        myService = MyService()
+    }
+
+    @Test
+    fun testAction() {
+        assertEquals("Action Done!", myService.performAction())
+    }
+}
