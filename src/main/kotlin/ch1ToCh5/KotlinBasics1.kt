2440f84e553c09bd135b4a7a3b0e094cba2d3550
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-24 18:18:08 +0300

    async/await

--- src/main/kotlin/ch1ToCh5/KotlinBasics1.kt
+++ src/main/kotlin/ch1ToCh5/KotlinBasics1.kt
@@ -1,14 +1,45 @@
 package dev.dead.ch1ToCh5
 
-fun main() {
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
+suspend fun main() {
 
     val p1 = MyPerson("Joe")
     p1.isStudent = true
     println(p1.isStudent)
     val r1 = MyRectangle(10.5, 10.5)
     println("is Square = ${r1.isSquare}")
+    withContext(Dispatchers.Default) {
+        println("Starting...")
+
+        // Launch two async coroutines (like async/await in JS/Python)
+        val a = async { delay(1500) }.await()
+        println(a)
+
+        val first = async { waitFive("First") }
+        val second = async { waitFive("Second") }
+
+        // Await results (like Promise.await())
+        val result1 = first.await()
+        val result2 = second.await()
+
+        println("Results: $result1 and $result2")
+        println("Done.")
+    }
+    println("Main Thread = ${Thread.currentThread().name} is Done")
+
+
 
 }
+
+suspend fun waitFive(name: String): String {
+    delay(2000) // suspends coroutine without blocking a thread
+    return "Hello from $name"
+}
+
 
 class MyPerson {
     val name: String
