91fb08b4c6b399c7958fb66e06ead3b683e1d49d
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-28 18:48:43 +0300

    kotlin in action ch2 - add utility functions for character recognition, safe division, and enhance iteration examples in main

--- src/main/kotlin/ch2/IternationsDemo2.kt
+++ src/main/kotlin/ch2/IternationsDemo2.kt
@@ -18,7 +18,36 @@ fun main() {
     for (i in l) {
         println(i)
     }
+    fun isLetter(c: Char): Boolean = c in 'a'..'z' || c in 'A'..'Z'
+    fun isDigit(c: Char): Boolean = c in '0'..'9'
+
+    fun recognize(c: Char): String = when (c) {
+        in '0'..'9' -> "digit"
+        in 'a'..'z', in 'A'..'Z' -> "letter"
+        else -> "default"
+
+    }
+
     println("*".repeat(20))
     for (i in 1..3)
         println(fizzBuzz(i) + "\t" + "->$i")
+    println("*".repeat(20))
+    val listWtihIndex = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    for ((index, value) in listWtihIndex.withIndex()) {
+        println("$index: $value")
+    }
+    val num1 = divieBy(10, 3)
+    println(num1)
+    val num2 = divieBy(10, 0)
+    println(num2 ?: 999999999999)
+    
+}
+
+fun divieBy(i: Int, j: Int): Int? {
+    val a = try {
+        i / j
+    } catch (e: Exception) {
+        null
+    }
+    return a
 }
