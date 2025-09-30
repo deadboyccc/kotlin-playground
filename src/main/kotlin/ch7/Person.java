bfb8518841cbf2f54123771cf69dd4ec10fbde2e
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-10-01 01:17:52 +0300

    kotlin in action ch7 - add examples of nullability handling: safe call operator, Elvis operator, let function, platform types, not-null assertion, lateinit properties, safely casting values, and extending nullable types

---
+++ src/main/kotlin/ch7/Person.java
@@ -0,0 +1,14 @@
+package ch7;
+
+/* Java */
+public class Person {
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
